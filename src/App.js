42b41ca3687c7e2e877494c211622f20f6377a0c
Eriche Gonzales <erichelouise@gmail.com>
2022-07-19 09:39:21 -0400

    Made backgrounds and buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
+import MainMenu from './components/MainMenu'
+import CreateACharacter from './components/CreateACharacter'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  // This syntax is called JSX = JavaScript XML
+
+  // useState is a function that returns an array of two elements
+  // returns a state variable and a function
+  // to update it
+  // TERNARY OPERATOR:
+  // (condition) ? return this if true : return this if false
+  // <Home />  ... const Home = {html: [], change: () => {}}
+  const [screen, setScreen] = useState('main')
+  
+  return(
+    <div>
+      {
+        (screen === 'main') ? <MainMenu setScreen={setScreen} /> : <CreateACharacter />
+      }
     </div>
   );
 }
